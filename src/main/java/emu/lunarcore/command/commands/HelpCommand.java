b67d12422f448ce28915a40fdb0ce71862ed19d5
QiuSYan <halo-402@outlook.com>
2024-05-26 10:27:20 +0800

    更改/help指令的提示

--- src/main/java/emu/lunarcore/command/commands/HelpCommand.java
+++ src/main/java/emu/lunarcore/command/commands/HelpCommand.java
@@ -11,6 +11,12 @@ public class HelpCommand implements CommandHandler {
     @Override
     public void execute(CommandArgs args) {
         args.sendMessage("以下是所有可用指令:");
+        args.sendMessage("注意：");
+        args.sendMessage("大括号 {} 是在其中选择一个参数填入");
+        args.sendMessage("中括号 [] 是必须填入此参数");
+        args.sendMessage("小括号 () 是可选填入的参数");
+        args.sendMessage("--------------");
+        args.sendMessage("指令生成工具：https://sr-tool.qiusyan.top，欢迎使用");
         
         // Sort command names
         var labels = LunarCore.getCommandManager().getLabels().keySet().stream().sorted().toList();
